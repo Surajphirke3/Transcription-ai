3e92b1a6aefba99c65aa467c7416f84036cb57be
Surajphirke3 <surajphirke3@gmail.com>
2025-06-23 18:35:31 +0530

    initial setup

--- forge.config.ts
+++ forge.config.ts
@@ -1,33 +1,53 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { VitePlugin } from "@electron-forge/plugin-vite";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    asar: true,
+  },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
+  ],
   plugins: [
-    new WebpackPlugin({
-      mainConfig,
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
-            preload: {
-              js: './src/preload.ts',
-            },
-          },
-        ],
-      },
+    new VitePlugin({
+      build: [
+        {
+          entry: "src/main.ts",
+          config: "vite.main.config.ts",
+          target: "main",
+        },
+        {
+          entry: "src/preload.ts",
+          config: "vite.preload.config.ts",
+          target: "preload",
+        },
+      ],
+      renderer: [
+        {
+          name: "main_window",
+          config: "vite.renderer.config.mts",
+        },
+      ],
+    }),
+
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
 };
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
+import { Button } from "./components/ui/button";
 
 const App = () => (
   <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -13,12 +14,17 @@ const App = () => (
         TypeScript, and Tailwind CSS, utilizing Electron Forge for easy
         packaging and publishing.
       </p>
+      <Button>
+        hello world
+      </Button>
     </div>
   </div>
 );
 
 function render() {
-  const root = ReactDOM.createRoot(document.getElementById("app"));
+  const rootElement = document.getElementById("app");
+  if (!rootElement) throw new Error("Failed to find the root element");
+  const root = ReactDOM.createRoot(rootElement);
   root.render(<App />);
 }
 
